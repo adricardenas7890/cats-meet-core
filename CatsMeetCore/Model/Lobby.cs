11121b52c56ecb5336d155721b2ac37774f9513e
Adriana Cardenas <acardenas@Adrianas-MacBook-Pro.local>
2020-07-25 00:58:09 -0500

    added further file structure

---
+++ CatsMeetCore/Model/Lobby.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CatsMeetCore.Model 
+{
+
+    public class Lobby 
+    {
+        [Key] // Adds ID as identity column. Will create this ID automatically
+        public int ID {get;set;}
+
+        [Required] // Name cannot be null
+        public string Name {get;set;}
+        public string Picture {get;set;}
+        public string Mood {get;set;}
+        public string Status {get;set;}
+    }
+}
